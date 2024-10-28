b870e819e4a8c39e93b81bff22a07970a9767f09
shuchitachawla2909 <shuchita.chawla.cd.cse23@itbhu.ac.in>
2024-10-28 10:27:55 +0530

    add movie-3

--- itw2/users/models.py
+++ itw2/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE),
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
     image=models.ImageField(default='default.jpg',upload_to='profile_pics')
 
 def __str__(self):
