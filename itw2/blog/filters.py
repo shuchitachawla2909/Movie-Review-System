3b5401421b889eb516afedcf6f7c767e409fe684
Surabhi-Mdn <surabhi.madan05@gmail.com>
2024-11-05 14:46:18 +0530

    final changes

--- itw2/blog/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Movie
-
-class MovieFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
-
-    class Meta:
-        model = Movie
-        fields = ['title']
